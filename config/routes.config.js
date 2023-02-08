763c210720839ec2b1d00fb19b15618822021f52
pianattero <mariapianattero@gmail.com>
2023-02-08 01:34:48 +0100

    feat: remove username from model, add index&login route and view

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+
+const miscController = require ("../controllers/misc.controller");
 const authController = require ("../controllers/auth.controller");
 
+
 /* Main route*/ 
-router.get("/", (req, res, next) => res.send("Welcome to AppSign"))
+router.get("/", miscController.index);
+
 /*Auth */
 router.get("/signup", authController.signup);
-router.post("signup", authController.doSignup);
+router.post("/signup", authController.doSignup);
+
+router.get("/login", authController.login);
 
 
 module.exports = router;
--- models/User.model.js
+++ models/User.model.js
@@ -1,65 +1,63 @@
-const mongoose = require ("mongoose");
-const bcrypt = require ("bcrypt");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const SALT_ROUNDS = 10;
 const EMAIL_PATTERN =
-/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_PATTERN = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-const userSchema = new mongoose.Schema (
-    {
-        username: {
-            type: String, 
-            required: [ true , "Please insert an User Name "], 
-            unique: [ true, "This User Name is already in use"],
-        }, 
-        firstName : {
-            type: String,
-            required: [true, "Please insert your first name"],
-        },
-        lastName : {
-            type: String,
-            required: [true, "Please insert your last name"],
-        }, 
-        dateOfBirth : {
-            type: Number,
-            required: true,
-        },
-        timeOfBirth: {
-            type: Number,
-            required: true,
-
-        }, 
-        email: {
-            type: String,
-            match: EMAIL_PATTERN,
-            required: [ true,"Please insert an email address" ], 
-            unique: [true, "Email is already in use"],
-        }, 
-        password: {
-            type: String,
-            required: [true, "Password is required"],
-            minlength: [8, "Your password must have at least 8 characters"]
-          },
-
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      match: [EMAIL_PATTERN, "Please, insert a valid email address"],
+      required: [true, "Please, insert an email address"],
+      unique: [true, "Email already in use"],
     },
-    {
-        timestamps: true,
-      }
-); 
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      match: [
+        PASSWORD_PATTERN,
+        "Your password must have a minimum of 8 characters, at least one letter and one number",
+      ],
+    },
+    firstName: {
+      type: String,
+      required: [true, "Please, insert your first name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please, insert your first name"],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    timeOfBirth: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.pre("save", function(next){
-    const rawPassword = this.password;
-    if (this.isModified('password')) {
-        bcrypt.hash(rawPassword, SALT_ROUNDS)
-          .then(hash => {
-            this.password = hash;
-            next()
-          })
-          .catch(err => next(err))
-      } else {
+userSchema.pre("save", function (next) {
+  const rawPassword = this.password;
+  if (this.isModified("password")) {
+    bcrypt
+      .hash(rawPassword, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
         next();
-      }
-})
+      })
+      .catch((err) => next(err));
+  } else {
+    next();
+  }
+});
 
-const User = mongoose.model("User", userSchema); 
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
