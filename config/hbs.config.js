5759068486dbefef1811c95914b89dd7e452b175
pianattero <mariapianattero@gmail.com>
2023-02-20 20:30:52 +0100

    feat: created mutualFollow helper

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,14 +1,27 @@
-const hbs = require('hbs');
-const path = require('path');
+const hbs = require("hbs");
+const path = require("path");
 
-hbs.registerPartials(path.join(__dirname, '../views/partials'));
+hbs.registerPartials(path.join(__dirname, "../views/partials"));
 
-hbs.registerHelper("isFollow", function (options){
-    const { userFollower, userFollowed } = options.hash;
-    if (userFollower.followeds.some(follow => follow.followed._id.toString() === userFollowed._id.toString())) {
-        return options.fn(this);
-    }
-    else {
-        return options.inverse(this);
-    }
-})
+hbs.registerHelper("isFollow", function (options) {
+  const { userFollower, userFollowed } = options.hash;
+
+  if (userFollower.followeds.some((follow) => follow.followed._id.toString() === userFollowed._id.toString())) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
+hbs.registerHelper("mutualFollow", function (options) {
+  const { userFollower, userFollowed } = options.hash;
+
+  const isUserFollowing = userFollower.followeds.some((follow) => follow.followed._id.toString() === userFollowed._id.toString())
+  const isUserFollowed = userFollower.followers.some((follow) => follow.follower._id.toString() === userFollowed._id.toString())
+
+  if (isUserFollowing && isUserFollowed) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
